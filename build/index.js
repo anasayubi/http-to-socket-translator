e04e67b1f9e56a8b6757f4d0e75ecf6505b6792c
anasayubi <anasayubi7152@gmail.com>
2019-02-25 10:24:13 +0500

    create http and io server

--- build/index.js
+++ build/index.js
@@ -1,8 +1,23 @@
 "use strict";
-// class http {
-//     constructor() {
-//         console.log("Hello World")
-//     }
-// }
-console.log("ana");
+// var app = require('express')();
+// var server = require('http').Server(app);
+// var io = require('socket.io')(server);
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var http_1 = require("http");
+var SocketIO = require("socket.io");
+var app = express();
+var httpServer = new http_1.Server(app);
+var ioServer = SocketIO(httpServer);
+console.log("Server listening on port 9001");
+httpServer.listen(9001);
+app.use(function (req, res, next) {
+    console.log("Incoming request");
+    console.log(req.path);
+    res.status(200).send("Ok");
+    // ioServer.emit("")
+});
+ioServer.on('connection', function (socket) {
+    console.log("Incoming connection");
+});
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,10 +1,26 @@
-// class http {
+// var app = require('express')();
+// var server = require('http').Server(app);
+// var io = require('socket.io')(server);
 
-//     constructor() {
-//         console.log("Hello World")
+import * as express from "express";
+import { Server } from "http";
+import * as SocketIO from "socket.io";
 
-//     }
+const app = express();
+let httpServer = new Server(app);
+let ioServer = SocketIO(httpServer);
 
-// }
+console.log("Server listening on port 9001")
+httpServer.listen(9001);
 
-console.log("ana")
+app.use((req, res, next) => {
+    console.log("Incoming request");
+    console.log(req.path);
+    res.status(200).send("Ok");
+    
+    // ioServer.emit("")
+});
+
+ioServer.on('connection', function(socket) {
+    console.log("Incoming connection");
+});
